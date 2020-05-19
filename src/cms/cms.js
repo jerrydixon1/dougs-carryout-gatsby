d162a7d50526210e2130ef2efbab5cfa9dd744d5
Jerry Dixon <jerrydixon1@gmail.com>
2020-05-18 23:20:05 -0400

    Attempting styled-components fix for preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,9 +2,10 @@ import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
 
+import withStyledComponentsRendered from './withStyledComponentsRendered'
 import IndexPagePreview from '@cms/preview-templates/IndexPagePreview'
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('index', withStyledComponentsRendered(IndexPagePreview))
